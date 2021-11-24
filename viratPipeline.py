f24abb748bd10af33f7dcbac02ce04e08907faf1
dyboyce <dylanboyce.206@gmail.com>
2021-11-24 12:27:11 -0800

    Initial commit

--- viratPipeline.py
+++ viratPipeline.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 
 def check_frame(curr_frame, objects_arr, events_arr, image):
@@ -32,23 +33,43 @@ def draw_box(s1, s2, x, y, image):
 
 
 def write_video():
-    pass
+    im_folder = '.'
+    video_name = 'recompiled.avi'
+    #os.chdir("C:\User\dylan\Documents\\viratDEMO")
+    images_arr = [img for img in os.listdir(im_folder)
+              if img.endswith(".jpg")]
+
+    vidframe = cv2.imread(os.path.join(im_folder, images_arr[0]))
+    height, width, layers = vidframe.shape
+
+    video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+
+    for image in images_arr:
+        video.write(cv2.imread(os.path.join(im_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
 
 
-# main function part of script
+    # main function part of script
 
-vidcap = cv2.VideoCapture('VIRAT_S_000200_01_000226_000268.mp4')
+vidcap = cv2.VideoCapture('VIRAT_S_000207_04_000902_000934.mp4')
 success, image = vidcap.read()
 count = 0
 color = (255, 255, 255)
 thickness = 5
 
-read_in_boxes()
+vid_objects, vid_events = read_in_boxes()
 
 while success:
-    check_frame(count)
+    image = check_frame(count, vid_objects, vid_events, image)
 
     cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
     success, image = vidcap.read()
     print('Read a new frame: ', count)
     count += 1
+
+    if count > 120:
+        break
+
+write_video()
